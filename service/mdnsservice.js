4a9a8dc5de6370e28a963224a27ce86282a5571c
Florian Schaar <sch17040@spengergasse.at>
2020-02-25 14:35:32 +0100

    Added module for ip address retrieval

---
+++ service/mdnsservice.js
@@ -0,0 +1,28 @@
+var mdns = require('multicast-dns')()
+var ip = require('ip')
+
+mdns.on("query", query => {
+    if (query.questions[0] && query.questions[0].name === "lokalqueue._tcp.local") {
+        console.log(query);
+
+        mdns.respond({
+            answers: [
+                {
+                    name: "lokalqueue._tcp.local",
+                    type: "SRV",
+                    data: {
+                        port: 8888, // dynamic port
+                        weight: 0,
+                        priority: 10,
+                        target: ip.address() // local IP
+                    }
+                }, {
+                    name: "lokalqueue._tcp.local",
+                    type: "A",
+                    data: ip.adress(),
+                    ttl: 300
+                }
+            ]
+        });
+    }
+});
